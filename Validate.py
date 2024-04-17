a4681dc74320721bc89e19046b3bc15f674e5393
Zhastik <Magmashot12345@gmail.com>
2024-04-18 02:43:38 +0300

    aa

--- Validate.py
+++ Validate.py
@@ -4,18 +4,32 @@ from pydantic import BaseModel, Field, validator
 class add_iphone(BaseModel):
     id: int
     model: str
-    DNS: int = Field(ge=0, max_length=7)
-    Mvideo: int = Field(ge=0, max_length=7)
-    Eldorado: int = Field(ge=0, max_length=7)
-    MTS: int = Field(ge=0, max_length=7)
+    DNS: int = Field(ge=0)
+    Mvideo: int = Field(ge=0)
+    Eldorado: int = Field(ge=0)
+    MTS: int = Field(ge=0)
 
 class get_iphone(BaseModel):
     id: int
     model: str
-    DNS: int = Field(ge=0, max_length=7)
-    Mvideo: int = Field(ge=0, max_length=7)
-    Eldorado: int = Field(ge=0, max_length=7)
-    MTS: int = Field(ge=0, max_length=7)
+    DNS: int = Field(ge=0)
+    Mvideo: int = Field(ge=0)
+    Eldorado: int = Field(ge=0)
+    MTS: int = Field(ge=0)
+
+class update_iphone(BaseModel):
+    id: int
+    model: str
+    DNS: int = Field(ge=0)
+    Mvideo: int = Field(ge=0)
+    Eldorado: int = Field(ge=0)
+    MTS: int = Field(ge=0)
+
+class patch_iphone(BaseModel):
+    DNS: int = Field(ge=0)
+    Mvideo: int = Field(ge=0)
+    Eldorado: int = Field(ge=0)
+    MTS: int = Field(ge=0)
 
 # class shop(BaseModel):
 #    id: int
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from typing import List
 
-from Validate import add_iphone, get_iphone
+from Validate import add_iphone, get_iphone, update_iphone, patch_iphone
 from Database import iphone_database
 
 app = FastAPI(
@@ -20,9 +20,44 @@ def Catalog(limit: int = 1, offset: int = 5):
 
     return [iphone.get('model') for iphone in iphone_database if (limit <= iphone.get('id') <= offset)]  #Выводит названия моделей, если они входят в диапозон
 
-@app.post('/catalog')
+@app.post('/add_iphone')
 def add_iphone(new_iphone: List[add_iphone]):
     """Функция добавляет новый iphone"""
 
     iphone_database.extend(new_iphone)
     return {"status": 200, "data": iphone_database[-1]}
+
+@app.put('/update_iphone')
+def update_iPhone(put_iphone: List[update_iphone]):
+    """Функция обновляет всю информацию про модель в базе данных"""
+
+    for iphone in iphone_database:
+        if iphone.get('id') == put_iphone[0].id:
+            iphone_database[(put_iphone[0].id - 1)] = put_iphone[0]
+            return iphone_database
+
+@app.delete("/iphone/{iphone_id}")
+def delete_iPhone(iphone_id: int):
+    """Функция удаляет iphone по id"""
+
+    for iphone in iphone_database:
+        if iphone.get('id') == iphone_id:
+            iphone_database.remove(iphone)
+            return iphone_database
+
+@app.patch('/iphone/{iphone_id}')
+def patch_price(iphone_id: int, price: List[patch_iphone]):
+    """Функция меняет цену в базе данных, если поступила новая цена (если поступил 0, цена не будет меняться)"""
+
+    for iphone in iphone_database:
+        if iphone.get('id') == iphone_id:
+            iphone["DNS"] = price[0].DNS if price[0].DNS != 0 else iphone["DNS"]
+            iphone["Mvideo"] = price[0].Mvideo if price[0].Mvideo != 0 else iphone["Mvideo"]
+            iphone["Eldorado"] = price[0].Eldorado if price[0].Eldorado != 0 else iphone["Eldorado"]
+            iphone["MTS"] = price[0].MTS if price[0].MTS != 0 else iphone["MTS"]
+            return iphone_database
+
+
+
+
+
